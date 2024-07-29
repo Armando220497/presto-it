441ffe5e692de2b680a0f6db4e55201ca61b9a63
Armando Di Santo <armando.di.santo@hotmail.com>
2024-07-29 16:58:25 +0200

    navbar terminata

---
+++ main.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('#navbar');
+let links = document.querySelectorAll('.nav-link');
+let logoNavbar = document.querySelector('#logoNavbar')
+console.dir(logoNavbar);
+window.addEventListener('scroll', () => {
+    let scrolled = window.scrollY;
+
+    if (scrolled > 0) {
+        navbar.classList.remove('bg-black');
+        navbar.classList.add('bg-yellow');
+        navbar.style.height = '70px';
+        links.forEach((link) => {
+            link.style.color = 'var(--black)';
+        });
+        logoNavbar.src = 'http://127.0.0.1:5500/media/logo%20black.png'
+    } else {
+        navbar.classList.add('bg-black');
+        navbar.classList.add('bg-yellow');
+        navbar.style.height = '140px';
+        links.forEach((link) => {
+            link.style.color = 'var(--yellow)';
+
+        });
+        logoNavbar.src = 'http://127.0.0.1:5500/media/logo%20giallo.png'
+    }
+});
